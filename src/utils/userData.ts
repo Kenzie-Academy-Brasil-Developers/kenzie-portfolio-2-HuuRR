58f8e94a174e7f197c41c4f0b07a8def6fdcb74c
Felipe Campregher <felipe.campregher@gmail.com>
2022-07-31 19:20:36 -0300

    edição realizada

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Felipe José Campregher",
+  githubUser: "HuuRR",
+  linkedinUser: "campregher",
+  whatsappNumber: "+5547991237055",
+  emailUser: "felipe.campregher@gmail.com",
+  instagramUser: "jcfeelipe",
   facebookUser: "",
 };
